368d9e5427039a64083074af485757b21b6214a5
Tadeu Luis <anestesya@gmail.com>
2010-07-14 01:42:22 -0300

    adicionado esquema lógico para as cenas

--- main.rb
+++ main.rb
@@ -2,19 +2,39 @@
 #ele vai armazenar os resultados do Campeonato de Ping Pong da Guenka
 #autor: anesteysa@gmail.com
 
-#arquivo que mostra o layout das tabelas
-#ele é responsável por organizar os resultados.
-
 require 'rubygems'
 require 'sinatra'
 require 'twitter'
 
+#variável global com os jogadores
+$jogadores = ['tadeu', 'daniel', 'mateus', 'brahim', 'heber', 'renan', 'bruno', 'laerte',
+               'jandira', 'gledston', 'cris', 'pedro', 'alessandro', 'Helder'];
+
+#página index.
 get '/' do
-    
-    @camp = 'Ping Pong - Guenka Software'
-    @jogadorA = 'Tadeu'
-    @jogadorB = 'Daniel'
-    @resultA = @resultB = 0
-    
-    erb :index
+  erb :index
 end
+
+
+#página de duplas
+get '/duplas' do 
+    @camp = 'Ping Pong - Guenka Software'
+    @jogadorA = $jogadores[rand(8)]; @jogadorB = $jogadores[rand(8)]
+    @resultA = @resultB = 0
+    erb :duplas  
+end
+
+#página de um contra um 
+get '/single' do
+   erb :single
+end
+
+#mostra pontuação para as duplas
+get '/score_duplas' do
+  erb :score_duplas
+end
+
+#mostra pontuação para o um contra um 
+get '/score_single' do
+  erb :score_single
+end 
