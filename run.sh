0e95bbb519ae3f52ce7cf317003971a1be06b985
Tadeu Luis <anestesya@gmail.com>
2010-07-15 08:40:39 -0300

    script que executa o programa

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## executa o shotgun com o main na porta 3001
+
+HOST=0.0.0.0
+PORT=3001
+FILE='main.rb'
+ 
+   if[ -f $FILE ]; 
+    then
+     /usr/bin/shotgun -p $PORT -o $HOST $FILE
+   fi
+ 
