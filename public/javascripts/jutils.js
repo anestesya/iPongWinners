75d95db8fccd991724bb3219af9ccf0dd7db696b
Tadeu Luis <anestesya@gmail.com>
2010-07-19 11:01:41 -0300

    adicionado funções para criar cronômetro, ainda não ativada no código

--- public/javascripts/jutils.js
+++ public/javascripts/jutils.js
@@ -91,3 +91,69 @@ function mostraHora(seletor){
 				mostraHora("#tempo");
 			}, 1000);
 }//fim mostraHora()
+
+var timercount = 0;
+var timestart  = null;
+  
+function sw_start(){
+	if(!timercount){
+	timestart   = new Date();
+	document.timeform.timetextarea.value = "00:00";
+	document.timeform.laptime.value = "";
+	timercount  = setTimeout("mostraHora()", 1000);
+	}
+	else{
+	var timeend = new Date();
+		var timedifference = timeend.getTime() - timestart.getTime();
+		timeend.setTime(timedifference);
+		var minutes_passed = timeend.getMinutes();
+		if(minutes_passed < 10){
+			minutes_passed = "0" + minutes_passed;
+		}
+		var seconds_passed = timeend.getSeconds();
+		if(seconds_passed < 10){
+			seconds_passed = "0" + seconds_passed;
+		}
+		var milliseconds_passed = timeend.getMilliseconds();
+		if(milliseconds_passed < 10){
+			milliseconds_passed = "00" + milliseconds_passed;
+		}
+		else if(milliseconds_passed < 100){
+			milliseconds_passed = "0" + milliseconds_passed;
+		}
+		document.timeform.laptime.value = minutes_passed + ":" + seconds_passed + "." + milliseconds_passed;
+	}
+}
+ 
+function Stop() {
+	if(timercount) {
+		clearTimeout(timercount);
+		timercount  = 0;
+		var timeend = new Date();
+		var timedifference = timeend.getTime() - timestart.getTime();
+		timeend.setTime(timedifference);
+		var minutes_passed = timeend.getMinutes();
+		if(minutes_passed < 10){
+			minutes_passed = "0" + minutes_passed;
+		}
+		var seconds_passed = timeend.getSeconds();
+		if(seconds_passed < 10){
+			seconds_passed = "0" + seconds_passed;
+		}
+		var milliseconds_passed = timeend.getMilliseconds();
+		if(milliseconds_passed < 10){
+			milliseconds_passed = "00" + milliseconds_passed;
+		}
+		else if(milliseconds_passed < 100){
+			milliseconds_passed = "0" + milliseconds_passed;
+		}
+		document.timeform.timetextarea.value = minutes_passed + ":" + seconds_passed + "." + milliseconds_passed;
+	}
+	timestart = null;
+}
+ 
+function Reset() {
+	timestart = null;
+	document.timeform.timetextarea.value = "00:00";
+	document.timeform.laptime.value = "";
+}
